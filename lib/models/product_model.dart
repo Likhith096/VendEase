ac23927c685c5d9e150014db676d2ad3dcf2bfa8
KshitijS093 <kshitij.cs21@bmsce.ac.in>
2024-01-31 01:41:54 +0530

    Dff

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,11 +1,10 @@
 class Product {
+  final String id;
   final String name;
   final double price;
   final double weight;
 
-  Product({
-    required this.name,
-    required this.price,
-    required this.weight,
-  });
+  Product({required this.id, required this.name, required this.price, required this.weight});
+
+  // Add other necessary fields and methods
 }
