96d3dfe8862c1e75f89ef93c75da7fb7565158a4
KshitijS093 <kshitij.cs21@bmsce.ac.in>
2024-02-11 20:16:34 +0530

    Carts Page Updated

--- lib/presentation/carts_page/carts_page.dart
+++ lib/presentation/carts_page/carts_page.dart
@@ -3,70 +3,243 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../../core/app_export.dart';
 import '../../widgets/custom_elevated_button.dart';
-// Assuming ProductlistItemWidget is correctly defined elsewhere
 
 class ProductlistsectionItemWidget extends StatelessWidget {
   final String productName;
   final String productPrice;
-  final String quantity;
   final String imageUrl;
+  int quantity;
+  final VoidCallback onAdd;
+  final VoidCallback onRemove;
 
-  const ProductlistsectionItemWidget({
+  ProductlistsectionItemWidget({
     Key? key,
     required this.productName,
     required this.productPrice,
     required this.quantity,
     required this.imageUrl,
+    required this.onAdd,
+    required this.onRemove,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Widget to display each product in the cart
+    // Updated widget to include plus-minus functionality
     return ListTile(
       leading: imageUrl.isNotEmpty
           ? Image.network(imageUrl, width: 50, height: 50, fit: BoxFit.cover)
           : Icon(Icons.image, size: 50), // Placeholder icon in case of no image URL
       title: Text(productName),
       subtitle: Text('Price: $productPrice, Quantity: $quantity'),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: Icon(Icons.remove),
+            onPressed: onRemove,
+          ),
+          Text('$quantity'),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: onAdd,
+          ),
+        ],
+      ),
     );
   }
 }
 
-class CartsPage extends StatelessWidget {
+class CartsPage extends StatefulWidget {
   const CartsPage({Key? key}) : super(key: key);
 
-  double calculateTotalPrice(List<QueryDocumentSnapshot> cartItems) {
-    double totalPrice = 0.0;
-    for (var item in cartItems) {
-      final data = item.data() as Map<String, dynamic>;
-      final price = data['price'] as double;
-      final quantity = data['quantity'] as int;
-      totalPrice += price * quantity;
+  @override
+  _CartsPageState createState() => _CartsPageState();
+}
+
+class _CartsPageState extends State<CartsPage> {
+  Future<List<QueryDocumentSnapshot>> fetchCartItems() async {
+  String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+  var querySnapshot = await FirebaseFirestore.instance
+      .collection('carts')
+      .where('userId', isEqualTo: userId)
+      .get();
+  return querySnapshot.docs; // This is inherently a List<QueryDocumentSnapshot>
+}
+
+
+
+  void updateCartQuantity(String docId, int change) async {
+  // Reference to the cart document
+  DocumentReference cartRef = FirebaseFirestore.instance.collection('carts').doc(docId);
+
+  // Assuming there's a straightforward way to reference the corresponding order document
+  // This might need adjustment based on your actual data model
+  DocumentReference orderRef = FirebaseFirestore.instance.collection('Orders').doc(docId);
+
+  FirebaseFirestore.instance.runTransaction((transaction) async {
+    // Get the current cart item snapshot
+    DocumentSnapshot cartSnapshot = await transaction.get(cartRef);
+
+    if (!cartSnapshot.exists) {
+      throw Exception("Cart item does not exist!");
     }
-    return totalPrice;
+
+    // Assuming the order item has a similar quantity field
+    // Get the current order item snapshot
+    DocumentSnapshot orderSnapshot = await transaction.get(orderRef);
+
+    if (!orderSnapshot.exists) {
+      throw Exception("Order item does not exist!");
+    }
+
+    // Update quantity in both cart and order documents
+    int newQuantity = (cartSnapshot['quantity'] as int) + change;
+    if (newQuantity > 0) {
+      transaction.update(cartRef, {'quantity': newQuantity});
+      transaction.update(orderRef, {'quantity': newQuantity}); // Update the order document similarly
+    } else {
+      // Optionally remove the item from both collections if its quantity reaches 0
+      transaction.delete(cartRef);
+      transaction.delete(orderRef); // Delete the order document similarly
+    }
+  }).then((result) {
+    // Refresh the UI after updating the documents
+    setState(() {});
+  }).catchError((error) {
+    print("Failed to update documents: $error");
+  });
+}
+
+
+  double calculateTotalPrice(List<QueryDocumentSnapshot> cartItems) {
+  double totalPrice = 0.0;
+  for (var item in cartItems) {
+    final data = item.data() as Map<String, dynamic>; // Correctly cast data
+    final double price = data['price'].toDouble(); // Ensure this is double and use toDouble() for safety
+    final int quantity = data['quantity'] as int;
+    totalPrice += price * quantity;
   }
+  return totalPrice;
+}
+
+
+//   void addProduct(String productId) async {
+//   print('Adding product: $productId');
+
+//   Product? productDetail = products.firstWhere(
+//     (p) => p.id == productId,
+//     orElse: () => Product(id: '', name: '', price: 0.0, weight: 0.0, imageURL: ''),
+//   );
+
+//   if (productDetail.id.isEmpty) {
+//     print('Product not found in the list');
+//     return;
+//   }
+
+//   setState(() {
+//     productQuantities[productId] = (productQuantities[productId] ?? 0) + 1;
+//   });
+
+//   String userId = getCurrentUserId();
+//   if (userId.isEmpty) {
+//     print('User ID is empty');
+//     return;
+//   }
+
+//   DocumentReference cartRef = FirebaseFirestore.instance.collection('carts').doc(productId);
+//   DocumentReference orderRef = FirebaseFirestore.instance.collection('Orders').doc(productId); // Reference to Orders collection
+
+//   FirebaseFirestore.instance.runTransaction((transaction) async {
+//     try {
+//       DocumentSnapshot cartSnapshot = await transaction.get(cartRef);
+//       DocumentSnapshot orderSnapshot = await transaction.get(orderRef); // Get the current order snapshot
+
+//       int newQuantity = 1;
+//       if (cartSnapshot.exists) {
+//         newQuantity = cartSnapshot['quantity'] + 1;
+//         transaction.update(cartRef, {'quantity': newQuantity});
+//       } else {
+//         transaction.set(cartRef, {
+//           'productId': productId,
+//           'userId': userId,
+//           'quantity': newQuantity,
+//           'price': productDetail.price,
+//           'name': productDetail.name,
+//           'imageURL': productDetail.imageURL,
+//           'timestamp': FieldValue.serverTimestamp(),
+//         });
+//       }
+
+//       // Update or set in the Orders collection similarly
+//       if (orderSnapshot.exists) {
+//         transaction.update(orderRef, {'quantity': newQuantity});
+//       } else {
+//         transaction.set(orderRef, {
+//           'productId': productId,
+//           'userId': userId,
+//           'quantity': newQuantity,
+//           'price': productDetail.price,
+//           'name': productDetail.name,
+//           'imageURL': productDetail.imageURL,
+//           'timestamp': FieldValue.serverTimestamp(),
+//         });
+//       }
+//     } catch (e) {
+//       print('Error updating cart and orders: $e');
+//     }
+//   });
+// }
+
+
+
+//   void removeProduct(String productId) async {
+//   if (!productQuantities.containsKey(productId) || productQuantities[productId]! <= 0) {
+//     print('Product quantity is already 0 or not in cart');
+//     return;
+//   }
+
+//   setState(() {
+//     productQuantities[productId] = productQuantities[productId]! - 1;
+//   });
+
+//   DocumentReference cartRef = FirebaseFirestore.instance.collection('carts').doc(productId);
+//   DocumentReference orderRef = FirebaseFirestore.instance.collection('Orders').doc(productId); // Reference to Orders collection
+
+//   FirebaseFirestore.instance.runTransaction((transaction) async {
+//     DocumentSnapshot cartSnapshot = await transaction.get(cartRef);
+//     DocumentSnapshot orderSnapshot = await transaction.get(orderRef); // Get the current order snapshot
+
+//     if (cartSnapshot.exists && cartSnapshot['quantity'] > 1) {
+//       int newQuantity = cartSnapshot['quantity'] - 1;
+//       transaction.update(cartRef, {'quantity': newQuantity});
+//       transaction.update(orderRef, {'quantity': newQuantity}); // Update Orders collection
+//     } else {
+//       transaction.delete(cartRef);
+//       transaction.delete(orderRef); // Remove from Orders collection if quantity becomes 0
+//     }
+//   });
+// }
+
 
   @override
-  Widget build(BuildContext context) {
-    final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
-    print("Attempting to fetch cart for User ID: $userId");
-    return SafeArea(
-      child: Scaffold(
-        body: FutureBuilder<QuerySnapshot>(
-          future: FirebaseFirestore.instance.collection('carts').where('userId', isEqualTo: userId).get(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
-              return const Text('Your cart is empty');
-            } else {
-              final totalPrice = calculateTotalPrice(snapshot.data!.docs);
-              return Column(
-                children: [
-                  // Light pink header with "Cart"
-                  Container(
+Widget build(BuildContext context) {
+  return SafeArea(
+    child: Scaffold(
+      body: FutureBuilder<List<QueryDocumentSnapshot>>(
+        future: fetchCartItems(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.data == null || snapshot.data!.isEmpty) {
+            return const Text('Your cart is empty');
+          } else {
+            final cartItems = snapshot.data!;
+            final totalPrice = calculateTotalPrice(cartItems);
+            return Column(
+              children: [
+                 Container(
                     width: double.infinity,
                     color: Colors.pink[100], // Light pink color
                     padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
@@ -79,30 +252,31 @@ class CartsPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        final data = snapshot.data!.docs[index].data() as Map<String, dynamic>;
-                        return ProductlistsectionItemWidget(
-                          productName: data['name'] ?? '',
-                          productPrice: data['price'].toString(),
-                          quantity: data['quantity'].toString(),
-                          imageUrl: data['imageURL'] ?? '', // Assuming imageURL fetching is correctly implemented
-                        );
-                      },
-                    ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: cartItems.length,
+                    itemBuilder: (context, index) {
+                      final data = cartItems[index].data() as Map<String, dynamic>;
+                      return ProductlistsectionItemWidget(
+                        productName: data['name'] ?? '',
+                        productPrice: data['price'].toString(),
+                        quantity: data['quantity'],
+                        imageUrl: data['imageURL'] ?? '',
+                        onAdd: () => updateCartQuantity(cartItems[index].id, 1),
+                        onRemove: () => updateCartQuantity(cartItems[index].id, -1),
+                      );
+                    },
                   ),
-                  _buildProceedToPaySection(context, totalPrice),
-                  const SizedBox(height: 9),
-                ],
-              );
-            }
-          },
-        ),
+                ),
+                _buildProceedToPaySection(context, totalPrice),
+              ],
+            );
+          }
+        },
       ),
-    );
-  }
+    ),
+  );
+}
 
  Widget _buildProceedToPaySection(BuildContext context, double totalPrice) {
   return Padding(
--- lib/presentation/orders_history/orders_history.dart
+++ lib/presentation/orders_history/orders_history.dart
@@ -1,7 +1,36 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import '../../core/app_export.dart';
+import '../../widgets/custom_elevated_button.dart';
+// Assuming ProductlistItemWidget is correctly defined elsewhere
+
+class ProductlistsectionItemWidget extends StatelessWidget {
+  final String productName;
+  final String productPrice;
+  final String quantity;
+  final String imageUrl;
+
+  const ProductlistsectionItemWidget({
+    Key? key,
+    required this.productName,
+    required this.productPrice,
+    required this.quantity,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Widget to display each product in the cart
+    return ListTile(
+      leading: imageUrl.isNotEmpty
+          ? Image.network(imageUrl, width: 50, height: 50, fit: BoxFit.cover)
+          : Icon(Icons.image, size: 50), // Placeholder icon in case of no image URL
+      title: Text(productName),
+      subtitle: Text('Price: $productPrice, Quantity: $quantity'),
+    );
+  }
+}
 
 class OrderHistoryScreen extends StatelessWidget {
   const OrderHistoryScreen({Key? key}) : super(key: key);
@@ -9,42 +38,50 @@ class OrderHistoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
-
+    print("Attempting to fetch orders for User ID: $userId");
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("Your Orders"),
-          backgroundColor: Colors.blue, // Customize your AppBar
-        ),
         body: FutureBuilder<QuerySnapshot>(
-          future: FirebaseFirestore.instance
-              .collection('orders') // Assuming 'orders' is your collection name
-              .where('userId', isEqualTo: userId)
-              .orderBy('date', descending: true) // Assuming you store a 'date' field in each order
-              .get(),
+          future: FirebaseFirestore.instance.collection('Orders').where('userId', isEqualTo: userId).get(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
+              return const CircularProgressIndicator();
             } else if (snapshot.hasError) {
-              return Center(child: Text('Error fetching orders'));
-            } else if (snapshot.data?.docs.isEmpty ?? true) {
-              return Center(child: Text('No orders found'));
+              return Text('Error: ${snapshot.error}');
+            } else if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
+              return const Text('You havenot Ordered anything till now');
             } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  final order = snapshot.data!.docs[index].data() as Map<String, dynamic>;
-
-                  // Formatting date for display, assuming 'date' is a Timestamp
-                  final DateTime orderDate = (order['date'] as Timestamp).toDate();
-                  final String formattedDate = DateFormat('dd MMM, yyyy').format(orderDate);
-
-                  return ListTile(
-                    title: Text('Order ID: ${snapshot.data!.docs[index].id}'),
-                    subtitle: Text('Date: $formattedDate - Total: \$${order['total']}'),
-                    // Add onTap or other widgets as needed, e.g., to view order details
-                  );
-                },
+              return Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    color: Colors.pink[100], // Light pink color
+                    padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                    child: Text(
+                      "Order History",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  // Your existing UI code...
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final data = snapshot.data!.docs[index].data() as Map<String, dynamic>;
+                        return ProductlistsectionItemWidget(
+                          productName: data['name'] ?? '',
+                          productPrice: data['price'].toString(),
+                          quantity: data['quantity'].toString(),
+                          imageUrl: data['imageURL'] ?? '', // Ensure this matches your field name
+                        );
+                      },
+                    ),
+                  ),
+                ],
               );
             }
           },
@@ -52,4 +89,10 @@ class OrderHistoryScreen extends StatelessWidget {
       ),
     );
   }
+
+  void onTapPtoP(BuildContext context) {
+    Navigator.pushNamed(context, AppRoutes.paymentScreen);
+  }
 }
+
+
